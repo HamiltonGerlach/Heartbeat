0aa0decef08a2d595abe586fa90f0b0b05676c03
Hamilton Gerlach <aboehmvie@gmail.com>
2021-08-11 21:48:46 +0200

    first testing revision

--- latch.ino
+++ latch.ino
@@ -1,5 +1,7 @@
 #include "InputDebounce.h"
 
+int LENGTHSCALE = 9.0;
+int OFFSET = 100.0;
 int SWITCH_DELAY = 50;
 int DURATION = 50;
 int PIN_OUT = 1;
@@ -7,16 +9,20 @@ int PIN_POT = 1;
 int PIN_SWITCH = 3;
 
 static InputDebounce Switch;
-bool Active = true;
+bool Active;
 
 // the setup routine runs once when you press reset:
 void setup() {                
     // initialize the digital pin as an output.
     pinMode(PIN_OUT, OUTPUT); //LED
+    // pinMode(2, INPUT);
+    // pinMode(3, INPUT_PULLUP);
     digitalWrite(PIN_OUT, LOW);
+    // digitalWrite(PIN_SWITCH, HIGH);
     
-    Switch.registerCallbacks(cbPressed, cbReleased, cbPressedDuration, cbReleasedDuration);
-    Switch.setup(PIN_SWITCH);
+    Switch.registerCallbacks(NULL, NULL, cbPressedDuration, cbReleasedDuration);
+    Switch.setup(PIN_SWITCH);//, 20, InputDebounce::PIM_INT_PULL_UP_RES, SWITCH_DELAY);
+    Active = true;
 }
 
 // the loop routine runs over and over again forever:
@@ -29,7 +35,7 @@ void loop() {
 
 int GetSpeed(int Pin) {
     int pot = analogRead(Pin);
-    float v = 100.0 * (1.0 + 5.0 * (pot / 1023.0));
+    float v = OFFSET * (1.0 + LENGTHSCALE * (pot / 1023.0));
     int out = round(v);
     return out;
 }
@@ -48,11 +54,14 @@ void Fire(int PulseDuration, int Speed) {
 
 
 void cbPressed(uint8_t pinIn) {
-    
+    // if (Active == true) {
+    //     int Speed = GetSpeed(PIN_POT);
+    //     Fire(DURATION, Speed);
+    // }
 }
 
 void cbReleased(uint8_t pinIn) {
-    
+    // Active = true;
 }
 
 void cbPressedDuration(uint8_t pinIn, unsigned long duration) {
